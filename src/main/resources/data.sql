72213b79a2afde2f36cc67ee1bf4717466f4bab5
NewtonJoaquim <newtonjoaquim12@gmail.com>
2021-07-13 21:39:28 -0300

    Definir permissões de endpoints baseado em Roles

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,11 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$.A6oO/N.iUrFiIz15uuIF.RK36UJeRK9Nqg.VjdWHJ78V6f9PH5je');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Moderador', 'moderador@email.com', '$2a$10$.A6oO/N.iUrFiIz15uuIF.RK36UJeRK9Nqg.VjdWHJ78V6f9PH5je');
+
+INSERT INTO PERFIL(id, nome) VALUES(1, 'ROLE_ALUNO');
+INSERT INTO PERFIL(id, nome) VALUES(2, 'ROLE_MODERADOR');
+
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES(1,1);	
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES(2,2);
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
