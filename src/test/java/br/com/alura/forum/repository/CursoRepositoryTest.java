a308b43098e31c52fd3bdcb024e402c7857e54de
NewtonJoaquim <newtonjoaquim12@gmail.com>
2021-07-16 22:19:11 -0300

    Adicionar suporte a variaveis de ambiente

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -1,16 +1,19 @@
 package br.com.alura.forum.repository;
 
-import org.junit.jupiter.api.Test;
 import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.alura.forum.modelo.Curso;
 
+@RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace= Replace.NONE)
 @ActiveProfiles("test")
@@ -23,7 +26,7 @@ public class CursoRepositoryTest {
 	private TestEntityManager em;
 	
 	@Test
-	void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
+	public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
 		Curso html5 = new Curso();
 		
 		String nomeCurso = "HTML 5";
@@ -36,7 +39,7 @@ public class CursoRepositoryTest {
 	}
 
 	@Test
-	void naoDeveriaCarregarUmCursoNaoCadastrado() {
+	public void naoDeveriaCarregarUmCursoNaoCadastrado() {
 		String nomeCurso = "asuhasiughad";
 		Curso curso = repository.findByNome(nomeCurso);
 		Assert.assertNull(curso);
